c261eba53b65f0a0bb745a6f9f3c10a3f4c8225c
Ariel Arreola <aarreola@slashweb.com.mx>
2026-03-21 00:52:42 -0600

    FK2C - Commit inicial + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center gap-6 py-8">
+        <h1 className="text-3xl font-bold leading-tight">Bienvenidos</h1>
+        <p className="text-md">Inventario Challenge</p>
+      </section>
+    </>
+  );
+}
+
+export default App;
